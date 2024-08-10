7441e562552ae53107b7a66b784fc5dee1d1de7e
Casey Bowen <casey.bowen64@gmail.com>
2024-08-10 13:45:45 -0500

    new column

--- SQL_Project_V1.session.sql
+++ SQL_Project_V1.session.sql
@@ -53,4 +53,58 @@ FROM customer_behavior
 GROUP BY memb
 ORDER BY "rounded age" ASC;
 
+--Are more men or women in each membership?
+--Firstly, we can see that there are exactly 175 Men and 175 women in the data set.
+
+SELECT COUNT(*), gender FROM customer_behavior
+GROUP BY gender
+
+SELECT COUNT(*), memb FROM customer_behavior
+WHERE gender LIKE 'Male'
+GROUP BY memb;
+
+SELECT COUNT(*), memb FROM customer_behavior
+WHERE gender LIKE 'Female'
+GROUP BY memb;
+
+/*After looking at the different breakdowns of behavior by gender,
+we see that more men purchase silver, whereas women are more likely to
+purchase either gold or bronze. */
+
+--How satisfied are each membership group with their purchase?
+--Does that change if they got a discount or not?
+
+SELECT COUNT(*), satisfaction
+FROM customer_behavior
+GROUP BY satisfaction
+LIMIT 10;
+
+/*To figure out the levels of satisfaction per group, I'm going to 
+transform the satisfaction column into an INT, where Unsatisfied = 1,
+Neutral = 2, Satisfied = 3, and NULL stays NULL. 
+
+Then, we can take the avg satisfaction INT per group, and use that to
+determine approximate levels of satisfaction. */
+
+ALTER TABLE customer_behavior ADD COLUMN sat_INT INT;
+UPDATE customer_behavior
+    SET sat_INT = CASE
+        WHEN satisfaction LIKE 'Unatisfied' THEN 1
+        WHEN satisfaction LIKE 'Neutral' THEN 2
+        WHEN satisfaction LIKE 'Satisfied' THEN 3
+        ELSE NULL
+    END;
+
+SELECT sat_INT FROM customer_behavior LIMIT 10;
+
+SELECT ROUND(AVG(sat_INT), 5), memb 
+FROM customer_behavior
+GROUP BY memb;
+
+/* Looks like Gold customers are really satisfied with thier products!
+Bronze is a solid neutral, and silver is a little above neutral.
+Looks like the area of opportunity lies in increasing satisfaction
+among our lower-tier members.
+
+Now let's look at how discounts play into this.
 
